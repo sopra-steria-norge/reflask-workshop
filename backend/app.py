6732d3001009431045acb019263feef4a7892688
Magnus Nermark <magnusnermark@gmail.com>
2020-11-12 13:55:47 +0100

    removed logger from main app and moved index-file into root of backend project

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,6 @@
 import os
-import logger
 
 MODE = os.getenv('FLASK_ENV')
-DEV_SERVER_URL = "http://localhost:3000"
-# logging.getLogger('werkzeug').disabled = True
+DEV_SERVER_URL = "http://localhost:3000/"
 
 print(MODE)
